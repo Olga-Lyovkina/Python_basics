cdb6e4ec1671d96fc40d81220e7ef3b2cc9a1936
Olga-Lyovkina <olyalyova@gmail.com>
2021-05-17 22:57:46 +0300

    GB_hw_lesson4: Выполнила 4 дз.

--- lesson4/2.py
+++ lesson4/2.py
@@ -1,6 +1,5 @@
 # task_2
 numbers_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-modified_list = [el for num, el in enumerate(numbers_list) if numbers_list[num - 1] < numbers_list[num]]
-modified_list.pop(0)
+modified_list = [numbers_list[num] for num in range(1, len(numbers_list)) if numbers_list[num - 1] < numbers_list[num]]
 print(f'Исходный список: {numbers_list}')
 print(f'Измененный список: {modified_list}')
